d8093d07cb94540637d236be371517bf4629b415
JuhMaran <julianemaran@gmail.com>
2026-04-29 17:36:48 -0300

    Exclui conteúdo de Carrinho Compra e comenta conteúdo do index php

--- app_carrinho_compras _refactored/index.php
+++ app_carrinho_compras _refactored/index.php
@@ -4,13 +4,12 @@ require __DIR__ . '/vendor/autoload.php';
 
 use AppCarrinhoCompras\CarrinhoCompra;
 
-// echo CarrinhoCompra::teste();
-
 echo '<h3>Com SRP</h3>';
-$carrinho1 = new CarrinhoCompra();
+
+/* $carrinho1 = new CarrinhoCompra();
 
 print_r($carrinho1->exibirItens());
-echo 'Valor total: ' . $carrinho1->exibirValorTotal();
+echo 'Valor total: ' . $carrinho1->exibirValorTotal(); */
 
 /* 
 $carrinho1->adicionarItem('Bicicleta', 750.10);
@@ -18,7 +17,7 @@ $carrinho1->adicionarItem('Geladeira', 1950.15);
 $carrinho1->adicionarItem('Tapete', 350.20); 
 */
 
-echo '<br/>';
+/* echo '<br/>';
 print_r($carrinho1->exibirItens());
 echo 'Valor total recalculado: ' . $carrinho1->exibirValorTotal();
 
@@ -34,4 +33,4 @@ if ($carrinho1->confirmarPedido()) {
     echo 'Erro ao confirmar pedido! Carrinho não possui itens.';
 }
 echo '<br/>';
-echo 'Status: ' . $carrinho1->exibirStatus();
+echo 'Status: ' . $carrinho1->exibirStatus(); */
