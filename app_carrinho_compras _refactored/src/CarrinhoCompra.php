531e718bd189051dbe86d09df2b770fbe72f34ce
JuhMaran <julianemaran@gmail.com>
2026-04-29 19:18:56 -0300

    Cria novos métodos para Carrinho de Compra

--- app_carrinho_compras _refactored/src/CarrinhoCompra.php
+++ app_carrinho_compras _refactored/src/CarrinhoCompra.php
@@ -2,4 +2,36 @@
 
 namespace AppCarrinhoCompras;
 
-class CarrinhoCompra {}
+use AppCarrinhoCompras\Item;
+
+class CarrinhoCompra
+{
+
+    // atributos
+    private $itens;
+
+    // construtor
+    public function __construct()
+    {
+        $this->itens = [];
+    }
+
+    // Recuperar os itens do carrinho
+    public function getItens()
+    {
+        return $this->itens;
+    }
+
+    // Adicionar um item ao carrinho
+    public function adicionarItem(Item $item)
+    {
+        array_push($this->itens, $item);
+        return true;
+    }
+
+    // Validar o carrinho de compras
+    public function validarCarrinho()
+    {
+        return count($this->itens) > 0;
+    }
+}
