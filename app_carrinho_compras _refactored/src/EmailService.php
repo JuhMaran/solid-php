e618a83b8fb3bd83ecc176c9532968470c702221
JuhMaran <julianemaran@gmail.com>
2026-04-29 17:24:41 -0300

    Cria projeto B

---
+++ app_carrinho_compras _refactored/src/EmailService.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace AppCarrinhoCompras;
+
+class EmailService {}
---
+++ app_carrinho_compras _refactored/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'julia/app_carrinho_compras',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'eb767a244057c9971556a9b208109591842ceca5',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'julia/app_carrinho_compras' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'eb767a244057c9971556a9b208109591842ceca5',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
