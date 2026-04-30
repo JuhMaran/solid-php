23be4940b55ec32cd4b61cf45436413b803cde3b
JuhMaran <julianemaran@gmail.com>
2026-04-30 15:45:51 -0300

    Implementa lógica no Email Service

--- app_carrinho_compras _refactored/src/EmailService.php
+++ app_carrinho_compras _refactored/src/EmailService.php
@@ -2,4 +2,29 @@
 
 namespace AppCarrinhoCompras;
 
-class EmailService {}
+class EmailService
+{
+
+    private $de;
+    private $para;
+    private $assunto;
+    private $conteudo;
+
+    public function __construct(
+        string $de = 'contato@site.com.br.',
+        string $para = '',
+        string $assunto = '',
+        string $conteudo = ''
+    ) {
+        $this->de = $de;
+        $this->para = $para;
+        $this->assunto = $assunto;
+        $this->conteudo = $conteudo;
+    }
+
+    public static function dispararEmail()
+    {
+        // Lógica para disparar o email
+        return "--- envia e-mail ---";
+    }
+}
