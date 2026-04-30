d239c60125b9a417fa7441c6377faa234e04fbc7
JuhMaran <julianemaran@gmail.com>
2026-04-30 12:46:01 -0300

    Implementação da classe Item

--- app_carrinho_compras _refactored/src/Item.php
+++ app_carrinho_compras _refactored/src/Item.php
@@ -2,4 +2,37 @@
 
 namespace AppCarrinhoCompras;
 
-class Item {}
+class Item
+{
+
+    private $descricao;
+    private $valor;
+
+    // Construtor
+    public function __construct()
+    {
+        $this->descricao = '';
+        $this->valor = 0;
+    }
+
+    // Getters e Setters
+    public function getDescricao()
+    {
+        return $this->descricao;
+    }
+
+    public function getValor()
+    {
+        return $this->valor;
+    }
+
+    public function setDescricao($descricao)
+    {
+        $this->descricao = $descricao;
+    }
+
+    public function setValor($valor)
+    {
+        $this->valor = $valor;
+    }
+}
