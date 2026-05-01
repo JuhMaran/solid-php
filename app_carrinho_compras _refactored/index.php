176e505965c0b8ad4bb3d84e82e368fb68c2f4e9
JuhMaran <julianemaran@gmail.com>
2026-04-30 22:51:09 -0300

    Index atualizado

--- app_carrinho_compras _refactored/index.php
+++ app_carrinho_compras _refactored/index.php
@@ -3,34 +3,69 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use AppCarrinhoCompras\CarrinhoCompra;
+use AppCarrinhoCompras\EmailService;
+use AppCarrinhoCompras\Item;
+use AppCarrinhoCompras\Pedido;
 
 echo '<h3>Com SRP</h3>';
 
-/* $carrinho1 = new CarrinhoCompra();
+$pedido = new Pedido();
 
-print_r($carrinho1->exibirItens());
-echo 'Valor total: ' . $carrinho1->exibirValorTotal(); */
+// Criar itens para o pedido
+$item1 = new Item();
+$item2 = new Item();
 
-/* 
-$carrinho1->adicionarItem('Bicicleta', 750.10);
-$carrinho1->adicionarItem('Geladeira', 1950.15);
-$carrinho1->adicionarItem('Tapete', 350.20); 
-*/
+$item1->setDescricao('Porta Copos');
+$item1->setValor(4.55);
 
-/* echo '<br/>';
-print_r($carrinho1->exibirItens());
-echo 'Valor total recalculado: ' . $carrinho1->exibirValorTotal();
+$item2->setDescricao('Lâmpada');
+$item2->setValor(8.32);
 
-echo '<br/>';
-echo 'Status: ' . $carrinho1->exibirStatus();
+// Exibir o pedido inicialmente, sem itens
+echo '<h4>Pedido Iniciado</h4>';
+echo '<pre>';
+print_r($pedido);
+echo '</pre>';
 
-$carrinho1->adicionarItem('Televisão 65"', 3570.25);
+// Adicionar itens ao carrinho de compras do pedido
+$pedido->getCarrinhoCompra()->adicionarItem($item1);
+$pedido->getCarrinhoCompra()->adicionarItem($item2);
 
-echo '<br/>';
-if ($carrinho1->confirmarPedido()) {
-    echo 'Pedido confirmado com sucesso!';
-} else {
-    echo 'Erro ao confirmar pedido! Carrinho não possui itens.';
+// Exibir o pedido após adicionar os itens
+echo '<h4>Pedido Com Itens</h4>';
+echo '<pre>';
+print_r($pedido);
+echo '</pre>';
+
+// Exibir informações de Itens
+echo '<h4>Itens do Carrinho</h4>';
+echo '<pre>';
+print_r($pedido->getCarrinhoCompra()->getItens());
+echo '</pre>';
+
+// Calcular o valor total do pedido
+echo '<h4>Valor do Total Pedido</h4>';
+$total = 0;
+foreach ($pedido->getCarrinhoCompra()->getItens() as $key => $item) {
+    $total += $item->getValor();
 }
-echo '<br/>';
-echo 'Status: ' . $carrinho1->exibirStatus(); */
+echo $total;
+
+// Validação do carrinho 
+echo '<h4>Carrinho está válido?</h4>';
+echo $pedido->getCarrinhoCompra()->validarCarrinho();
+
+// Status do Pedido
+echo '<h4>Status do Pedido</h4>';
+echo $pedido->getStatus();
+
+// Confirmar o pedido
+echo '<h4>Confirmar Pedido</h4>';
+echo $pedido->confirmar();
+
+// E-mail de confirmação
+echo '<h4>E-mail</h4>';
+if ($pedido->getStatus() == 'confirmado') {
+    // ação
+    echo EmailService::dispararEmail();
+}
